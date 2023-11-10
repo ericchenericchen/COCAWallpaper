564b555892f5df98ed0ec87c37528f42eb56a146
Eric Chen <ericsc@usc.edu>
2023-11-09 20:57:05 -0700

    yeah we're weathering

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,18 +1,21 @@
 import requests
 import json
 
+# OpenWeatherMap API: https://openweathermap.org/current
+OWM_API_KEY = 'e8463a344affe52b5803a9be423491e0' #MY API KEY DON'T SPAM OVER 1000 PLEASE :)
 
-# Here's some code i ripped from EE250 hopefully it helps with weather
-# # OpenWeatherMap API: https://openweathermap.org/current
+#Oslo Norway
+DEFAULT_LAT = 59.9139
+DEFAULT_LON = 10.7522
 
-OWM_API_KEY = ''  # OpenWeatherMap API Key, can get that
+#Next step do command line arguments for latitude and longitude so that we can customize the location
+#better.
 
-DEFAULT_ZIP = 90089
-
-def get_weather(zip_code):
+def get_weather(lat, lon):
     params = {
         'appid': OWM_API_KEY,
-        'zip': 90089,
+        'lat': lat,
+        'lon': lon, 
         'units': 'imperial'
     }
 
@@ -21,37 +24,32 @@ def get_weather(zip_code):
     if response.status_code == 200: # Status: OK
         data = response.json()
 
-        # print the json to parse what i need
+        # # PRINTS THE WHOLE JSON, UNCOMMENT IF YOU WANT TO FIND OTHER INFO
         # print(json.dumps(data, sort_keys=True, indent=4))
 
         temperature = data["main"]["temp"]
         humidity = data["main"]["humidity"]
+        location = data["name"]
+        weather = data["weather"][0]["description"]
+        gust = data["wind"]["gust"]
 
-        #test types of temp and humidity
-        # print(type(temperature), ' ', temperature)
-        # print(type(humidity), " ", humidity)
-        return (temperature, humidity)
+        return (temperature, humidity, location, weather, gust)
 
     else:
         print('error: got response code %d' % response.status_code)
         print(response.text)
-        return 0.0, 0.0
+        return 0.0, 0.0, "N/A", "N/A", 0.0
 
-def weather_init():
-    zip_code = DEFAULT_ZIP
-    temp, hum = get_weather(zip_code)
+def weather_init(lat=DEFAULT_LAT, lon=DEFAULT_LON):
+    # Working parameters
+    # temperature, humidity, location, general weather, wind speed
+    temp, hum, city, weather, gust = get_weather(lat, lon)
     
-    output = '{:.1f}F, {:>.0f}% humidity'.format(temp, hum)
-    print('weather for {}: {}'.format(zip_code, output))
+    output = f'{temp}F, {hum}% humidity, {gust}mph gust'
+    print(f'weather for {city}: {output}')
+    print(f'description: {weather}')
 
     return output
-
-
-WEATHER_APP = {
-    'name': 'Weather',
-    'init': weather_init
-}
-
 
 if __name__ == '__main__':
     weather_init()
